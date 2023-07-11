544ff7d31b5ceb1b7616d5745eaa6c78fe9e6b97
Siddhartha <siddharthamaitra875@gmail.com>
2023-07-11 12:23:06 +0530

    Commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    cin>>a;
+    cout<<"The valye of a is: "<<a<<endl;
+    cin>>b;
+    cout<<"The value of b: "<<b<<endl;
+
+    sum = a + b;
+    cout<<"The sum is: "<<sum<<endl;
+
+    return 0;
+}
